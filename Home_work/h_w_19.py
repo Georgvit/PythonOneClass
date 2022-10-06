2d4c8f7ec095a2f607e160d7fab51bf9c180a267
Georgvit <ifreyd@mail.ru>
2022-10-06 22:55:31 +0300

    не повторяющиеся элементы

--- Home_work/h_w_19.py
+++ Home_work/h_w_19.py
@@ -2,15 +2,22 @@
 # Напишите программу, которая выведет список
 # неповторяющихся элементов исходной последовательности.
 
+
+# Если в задаче имелось ввиду создать список в котором элементы не повторяются
+# Пользовательский ввод
 # number_list = input('Задайте последовательность чисел через пробел\n и нажмите ввод: ').split()
+# Преобразование строковых данных в тип int
 # number_list = [int(x) for x in number_list]
 #
 # print(f'Введенная последовательность: {number_list}')
+# Сортировка введенной последовательности
 # number_list.sort()
 #
+# Создание нового списка
 # sort_number_list = []
 # length_list = len(number_list) - 1
-#
+
+# Выборка
 # for i in range(length_list):
 #     if number_list[i] != number_list[i + 1]:
 #         sort_number_list.append(number_list[i])
@@ -18,20 +25,32 @@
 # if (sort_number_list[len(sort_number_list) - 1]) != number_list[len(number_list) - 1]:
 #     sort_number_list.append(number_list[len(number_list) - 1])
 #
-# print(f'Список неповторяющихся элементов: {sort_number_list}')
+# Вывод результата
+# print(f'Список из неповторяющихся элементов: {sort_number_list}')
 
+
+# Если в задаче имелось ввиду создать список в который вносятся элементы НЕ повторяющиеся в оригинальном списке
+# Пользовательский ввод
 number_list = input('Задайте последовательность чисел через пробел\n и нажмите ввод: ').split()
+# Преобразование строковых данных в тип int
 number_list = [int(x) for x in number_list]
 
 print(f'Введенная последовательность: {number_list}')
+# Сортировка введенной последовательности
 number_list.sort()
 
+# Создание нового списка
 sort_number_list = []
-leng = len(number_list) - 1
-count = 0
-while count > leng:
-    if number_list[count] == number_list[count + 1]:
-        del number_list[count:count+1]
+
+# Выборка
+for i in range(len(number_list)):
+    count = 0
+    for j in range(len(number_list)):
+        if number_list[i] == number_list[j]:
+            count += 1
+    if count == 1:
+        sort_number_list.append(number_list[i])
 
 
-print(number_list)
+# Вывод результата
+print(f'Список из неповторяющихся элементов: {sort_number_list}')
